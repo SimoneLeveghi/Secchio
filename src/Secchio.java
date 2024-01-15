52497c534c1182561109d0a8fe9a35c0421f022d
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-01-15 10:07:23 +0100

    Implemented methods riempi(), svuota(), travasa() and Interfaces Comparable, Serializable and Cloneable

--- src/Secchio.java
+++ src/Secchio.java
@@ -1,4 +1,8 @@
-public class Secchio {
+import com.google.gson.Gson;
+
+import java.io.Serializable;
+
+public class Secchio implements Cloneable, Comparable<Secchio>, Serializable {
     private double volume;
     private double capacita;
 
@@ -6,6 +10,15 @@ public class Secchio {
         if(volume <= 0 || capacita <= 0) throw new Exception("Valori non validi");
         this.volume = volume;
         this.capacita = capacita;
+    }
+
+    public Secchio(Secchio s) {
+        this.volume = s.volume;
+        this.capacita = s.capacita;
+    }
+
+    public Secchio(String json) {
+        this(new Gson().fromJson(json, Secchio.class));
     }
 
     public double getVolume() {
@@ -18,5 +31,46 @@ public class Secchio {
 
     public void setVolume(double volume) {
         this.volume = volume;
+    }
+
+    public double riempi() {
+        this.volume = this.capacita;
+    }
+
+    public double riempi(double qty) {
+        return (volume += (Math.min(qty, capacita - volume)));
+    }
+
+    public void svuota() {
+        this.volume = 0;
+    }
+
+    public void svuota(double qty) {
+        this.volume -= Math.min(qty, volume);
+    }
+
+    public void travasa(Secchio s) {
+        this.volume = s.riempi(volume);
+    }
+
+    public void travasa(Secchio s, double qty) {
+        double quantitaTravaso = Math.min(qty, volume);
+        this.volume -= quantitaTravaso;
+        s.riempi(quantitaTravaso);
+    }
+
+    @Override
+    public Secchio clone() {
+        return new Secchio(this);
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+
+    @Override
+    public int compareTo(Secchio s) {
+        return (int) (this.capacita - s.capacita);
     }
 }
