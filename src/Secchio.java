640580c940539d9d115429fd22dab0f9e7c8af13
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-01-15 09:41:06 +0100

    Add basic methods to class Secchio

--- src/Secchio.java
+++ src/Secchio.java
@@ -1,2 +1,22 @@
 public class Secchio {
+    private double volume;
+    private double capacita;
+
+    public Secchio(double volume, double capacita) throws Exception{
+        if(volume <= 0 || capacita <= 0) throw new Exception("Valori non validi");
+        this.volume = volume;
+        this.capacita = capacita;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public double getCapacita() {
+        return capacita;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
 }
