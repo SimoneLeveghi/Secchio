be4f11f07099ebf684c4f8ac2f3d9b2b741c01d8
Simon9116 <simoneleveghi8@protonmail.com>
2024-01-23 19:20:49 +0100

    Update tests in main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            Secchio s1 = new Secchio(5.5, 6);
+            Secchio s1 = Secchio.generaSecchioRandom();
             Secchio s2 = new Secchio(s1);
-            Secchio s3 = new Secchio(0.5, 7);
+            Secchio s3 = Secchio.generaSecchioRandom();
 
             s1.travasa(s2, 0.2);
             System.out.println("s1 volume: " + s1.getVolume());
+            System.out.println("s1 capacità: " + s1.getCapacita());
             System.out.println("s2 volume: " + s2.getVolume());
+            System.out.println("s2 capacità: " + s2.getCapacita());
+            System.out.println("s3 volume: " + s3.getVolume());
+            System.out.println("s3 capacità: " + s3.getCapacita());
 
-            System.out.println("Compare: " + s1.compareTo(s2));
+            System.out.println("Compare s1 to s2: " + s1.compareTo(s2));
+            System.out.println("Compare s1 to s3: " + s1.compareTo(s3));
         }
         catch(Exception e) {
             System.out.println(e.getMessage());
